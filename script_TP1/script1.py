91e6072ce22ba289b5e7ca2f85dcfc57f39fb3a2
Enrico Calzavarini <ecalzavarini@gmail.com>
2016-09-19 10:37:33 +0200

    untrack scripts

--- script_TP1/script1.py
+++
@@ -1,3 +0,0 @@
-print("Bienvenue au premier TP d'informatique")
-n = input("Entrer le numero d'etudiantes :")
-print("Nous sommes " + str(n) + " etudiantes" )
--- script_TP2/script_prova_np.py
+++
@@ -1,6 +0,0 @@
-from numpy import *
-import matplotlib.pyplot as plt
-
-a=zeros((2,3), dtype=float)
-
-print a
